0bcbfedbaaafbc1499a8aceb4606046ca20d0e7f
d1ll0n <dillonkellar@protonmail.com>
2021-06-06 12:16:38 -0500

    fix interface

--- contracts/test/TestDNDX.sol
+++ contracts/test/TestDNDX.sol
@@ -10,7 +10,7 @@ contract TestDNDX {
 
   address public constant ndx = 0x86772b1409b61c639EaAc9Ba0AcfBb6E238e5F83;
 
-  function distributeDividends(uint256 amount) external {
+  function distribute(uint256 amount) external {
     ndx.safeTransferFrom(msg.sender, address(this), amount);
   }
 }

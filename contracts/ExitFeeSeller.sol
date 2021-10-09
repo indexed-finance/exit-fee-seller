825ae6d17a45f38d4d8811c01a7d507b7be6e491
d1ll0n <dillonkellar@protonmail.com>
2021-10-09 09:06:56 -0500

    Remove Bancor, add Sushi, use ETH as distribution token, add treasury share of revenue

--- contracts/ExitFeeSeller.sol
+++ contracts/ExitFeeSeller.sol
@@ -6,9 +6,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./libraries/TransferHelper.sol";
 import "./libraries/LowGasSafeMath.sol";
 import "./libraries/UniswapV2Library.sol";
-import "./interfaces/IBancorNetwork.sol";
+import "./libraries/PairsLibrary.sol";
 import "./interfaces/IContractRegistry.sol";
+import "./interfaces/IOracle.sol";
 import "./interfaces/IWETH.sol";
+import "./interfaces/IDNDX.sol";
 
 
 contract ExitFeeSeller is Ownable() {
@@ -19,18 +21,15 @@ contract ExitFeeSeller is Ownable() {
 
   uint256 public constant minTwapAge = 30 minutes;
   uint256 public constant maxTwapAge = 2 days;
-  address public constant bancorEthAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-  bytes32 public constant bancorName = 0x42616e636f724e6574776f726b00000000000000000000000000000000000000; // "BancorNetwork"
   IOracle public constant oracle = IOracle(0xFa5a44D3Ba93D666Bf29C8804a36e725ecAc659A);
-  IContractRegistry public constant bancorRegistry = IContractRegistry(0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4);
-  address public constant uniswapFactory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
-  address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-  address public constant ndx = 0x86772b1409b61c639EaAc9Ba0AcfBb6E238e5F83;
-  IDNDX public immutable dndx;
+  address public constant treasury = 0x78a3eF33cF033381FEB43ba4212f2Af5A5A0a2EA;
+  IWETH public constant weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+  IDNDX public constant dndx = IDNDX(0x262cd9ADCE436B6827C01291B84f1871FB8b95A3);
 
 /* ==========  Storage  ========== */
 
   uint16 public twapDiscountBips = 500; // 5%
+  uint16 public ethToTreasuryBips = 4000; // 40%
 
 /* ==========  Structs  ========== */
 
@@ -42,12 +41,6 @@ contract ExitFeeSeller is Ownable() {
     uint256 amountOut;
   }
 
-  struct BancorParams {
-    IBancorNetwork bancor;
-    address[] path;
-    uint256 amountOut;
-  }
-
 /* ==========  Fallbacks  ========== */
 
   fallback() external payable { return; }
@@ -55,11 +48,21 @@ contract ExitFeeSeller is Ownable() {
 
 /* ==========  Constructor  ========== */
 
-  constructor(address _dndx) {
-    dndx = IDNDX(_dndx);
-    ndx.safeApprove(_dndx, type(uint256).max);
+  constructor() {
+    weth.approve(address(dndx), type(uint256).max);
   }
 
+/* ==========  Token Transfer  ========== */
+
+  /**
+   * @dev Transfers full balance held by the owner of each provided token
+   * to the seller contract.
+   *
+   * Because the seller will have to be enabled through a proposal and will
+   * take several days to go into effect, it will not be possible to know the
+   * precise balance to transfer ahead of time; instead, infinite approval will
+   * be given and this function will be called to execute the transfers.
+   */
   function takeTokensFromOwner(address[] memory tokens) external {
     uint256 len = tokens.length;
     address _owner = owner();
@@ -74,133 +77,111 @@ contract ExitFeeSeller is Ownable() {
 
 /* ==========  Owner Controls  ========== */
 
+  /**
+   * @dev Sets the maximum discount on the TWAP that the seller will accept
+   * for a trade in basis points, e.g. 500 means the token must be sold
+   * for >=95% of the TWAP.
+   */
   function setTWAPDiscountBips(uint16 _twapDiscountBips) external onlyOwner {
-    require(_twapDiscountBips < 1000, "Can not set discount over 10%");
+    require(_twapDiscountBips <= 1000, "Can not set discount >= 10%");
     twapDiscountBips = _twapDiscountBips;
+  }
+
+  /**
+   * @dev Sets the portion of revenue that are received by the treasury in basis
+   * points, e.g. 4000 means the treasury gets 40% of revenue and dndx gets 60%.
+   */
+  function setEthToTreasuryBips(uint16 _ethToTreasuryBips) external onlyOwner {
+    require(_ethToTreasuryBips <= 10000, "Can not set bips over 100%");
+    ethToTreasuryBips = _ethToTreasuryBips;
+  }
+
+  /**
+   * @dev Return tokens to the owner. Can be used if there is a desired change
+   * in the revenue distribution mechanism.
+   */
+  function returnTokens(address[] memory tokens) external onlyOwner {
+    uint256 len = tokens.length;
+    address _owner = owner();
+    for (uint256 i = 0; i < len; i++) {
+      address token = tokens[i];
+      if (token == address(0)) {
+        uint256 bal = address(this).balance;
+        if (bal > 0) _owner.safeTransferETH(bal);
+      } else {
+        uint256 bal = IERC20(token).balanceOf(address(this));
+        if (bal > 0) token.safeTransfer(_owner, bal);
+      }
+    }
   }
 
 /* ==========  Queries  ========== */
 
-  function getBancor() public view returns (IBancorNetwork) {
-    return IBancorNetwork(bancorRegistry.addressOf(bancorName));
+  function getBestPair(address token, uint256 amount) public view returns (address pair, uint256 amountOut) {
+    bool zeroForOne = token < address(weth);
+    (address token0, address token1) = zeroForOne ? (token, address(weth)) : (address(weth), token);
+    uint256 amountUni;
+    uint256 amountSushi;
+    address uniPair = PairsLibrary.calculateUniPair(token0, token1);
+    address sushiPair = PairsLibrary.calculateSushiPair(token0, token1);
+    {
+      (uint256 reserve0, uint256 reserve1) = PairsLibrary.tryGetReserves(uniPair);
+      if (reserve0 > 0 && reserve1 > 0) {
+        (uint256 reserveIn, uint256 reserveOut) = zeroForOne ? (reserve0, reserve1) : (reserve1, reserve0);
+        amountUni = UniswapV2Library.getAmountOut(amount, reserveIn, reserveOut);
+      }
+    }
+    {
+      (uint256 reserve0, uint256 reserve1) = PairsLibrary.tryGetReserves(sushiPair);
+      if (reserve0 > 0 && reserve1 > 0) {
+        (uint256 reserveIn, uint256 reserveOut) = zeroForOne ? (reserve0, reserve1) : (reserve1, reserve0);
+        amountSushi = UniswapV2Library.getAmountOut(amount, reserveIn, reserveOut);
+      }
+    }
+    return amountUni >= amountSushi ? (uniPair, amountUni) : (sushiPair, amountSushi);
   }
 
-  function getBancorNDXForETHParams(uint256 amountIn)
-    public
-    view
-    returns (BancorParams memory params)
-  {
-    IBancorNetwork bancor = getBancor();
-    params.bancor = bancor;
-    params.path = bancor.conversionPath(bancorEthAddress, ndx);
-    params.amountOut = bancor.rateByPath(params.path, amountIn);
-  }
-
-  function getUniswapParams(
-    address tokenIn, 
-    address tokenOut, 
-    uint256 amountIn
-  ) public view returns (UniswapParams memory params) {
-    (address token0, address token1) = UniswapV2Library.sortTokens(tokenIn, tokenOut);
-    address pair = UniswapV2Library.calculatePair(uniswapFactory, token0, token1);
-    bool zeroForOne = tokenIn == token0;
-    (uint256 reserve0, uint256 reserve1, ) = IUniswapV2Pair(pair).getReserves();
-    (uint256 reserveIn, uint256 reserveOut) = tokenIn == token0
-      ? (reserve0, reserve1)
-      : (reserve1, reserve0);
-    uint256 amountOut = UniswapV2Library.getAmountOut(amountIn, reserveIn, reserveOut);
-    params = UniswapParams(tokenIn, amountIn, pair, zeroForOne, amountOut);
-  }
-
-  function getMinimumAmountOut(address tokenIn, address tokenOut, uint256 amountIn) public view returns (uint256) {
-    uint256 averageAmountOut = (tokenIn == weth)
-      ? oracle.computeAverageTokensForEth(tokenOut, amountIn, minTwapAge, maxTwapAge)
-      : oracle.computeAverageEthForTokens(tokenIn, amountIn, minTwapAge, maxTwapAge);
-
-    return averageAmountOut.sub(averageAmountOut.mul(twapDiscountBips) / uint256(10000));
+  function getMinimumAmountOut(address token, uint256 amountIn) public view returns (uint256) {
+    uint256 averageAmountOut = oracle.computeAverageEthForTokens(token, amountIn, minTwapAge, maxTwapAge);
+    return averageAmountOut.sub(mulBips(averageAmountOut, twapDiscountBips));
   }
 
 /* ==========  Swaps  ========== */
 
-  function execute(UniswapParams memory params) internal {
-    params.tokenIn.safeTransfer(params.pair, params.amountIn);
-    (uint256 amount0Out, uint256 amount1Out) = params.zeroForOne ? (uint256(0), params.amountOut) : (params.amountOut, uint256(0));
-    IUniswapV2Pair(params.pair).swap(amount0Out, amount1Out, address(this), new bytes(0));
+  function execute(address token, address pair, uint256 amountIn, uint256 amountOut) internal {
+    token.safeTransfer(pair, amountIn);
+    (uint256 amount0Out, uint256 amount1Out) = token < address(weth) ? (uint256(0), amountOut) : (amountOut, uint256(0));
+    IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), new bytes(0));
   }
 
   function sellTokenForETH(address token) external {
-    require(token != ndx, "Can not sell NDX");
-    require(token != weth, "Can not sell WETH");
-    uint256 amountIn = IERC20(token).balanceOf(address(this));
-    uint256 minimumAmountOut = getMinimumAmountOut(token, weth, amountIn);
-    UniswapParams memory params = getUniswapParams(token, weth, amountIn);
-    require(params.amountOut >= minimumAmountOut, "Insufficient output");
-    execute(params);
+    sellTokenForETH(token, IERC20(token).balanceOf(address(this)));
   }
 
-  function buyNDX() external {
-    uint256 wethBalance = IERC20(weth).balanceOf(address(this));
-    uint256 ethBalance = address(this).balance;
-    uint256 amountIn = wethBalance + ethBalance;
-    UniswapParams memory uniParams = getUniswapParams(weth, ndx, amountIn);
-    BancorParams memory bancorParams = getBancorNDXForETHParams(amountIn);
-    uint256 minimumAmountOut = getMinimumAmountOut(weth, ndx, amountIn);
-    // >= because uniswap costs less gas
-    if (uniParams.amountOut >= bancorParams.amountOut) {
-      require(uniParams.amountOut >= minimumAmountOut, "Insufficient output");
-      IWETH(weth).deposit{value: ethBalance}();
-      execute(uniParams);
-      dndx.distribute(uniParams.amountOut);
-    } else {
-      require(bancorParams.amountOut >= minimumAmountOut, "Insufficient output");
-      IWETH(weth).withdraw(wethBalance);
-      bancorParams.bancor.convertByPath{value: amountIn}(
-        bancorParams.path,
-        amountIn,
-        bancorParams.amountOut,
-        address(0),
-        address(0),
-        0
-      );
-      dndx.distribute(bancorParams.amountOut);
+  function sellTokenForETH(address token, uint256 amountIn) public {
+    require(token != address(weth), "Can not sell WETH");
+    uint256 minimumAmountOut = getMinimumAmountOut(token, amountIn);
+    (address pair, uint256 amountOut) = getBestPair(token, amountIn);
+    require(amountOut >= minimumAmountOut, "Insufficient output");
+    execute(token, pair, amountIn, amountOut);
+  }
+
+/* ==========  Distribution  ========== */
+
+  function distributeETH() external {
+    uint256 bal = address(this).balance;
+    if (bal > 0) weth.deposit{value: bal}();
+    bal = weth.balanceOf(address(this));
+    if (bal > 0) {
+      uint256 ethToTreasury = mulBips(bal, ethToTreasuryBips);
+      address(weth).safeTransfer(treasury, ethToTreasury);
+      dndx.distribute(bal - ethToTreasury);
     }
   }
-}
 
+/* ==========  Utils  ========== */
 
-interface IDNDX {
-  function distribute(uint256 amount) external;
-}
-
-
-interface IOracle {
-  struct PriceObservation {
-    uint32 timestamp;
-    uint224 priceCumulativeLast;
-    uint224 ethPriceCumulativeLast;
+  function mulBips(uint256 a, uint256 bips) internal pure returns (uint256) {
+    return a.mul(bips) / uint256(10000);
   }
-
-  function updatePrice(address token) external returns (bool);
-
-  function updatePrices(address[] calldata tokens) external returns (bool[] memory);
-
-  function getPriceObservationsInRange(
-    address token,
-    uint256 timeFrom,
-    uint256 timeTo
-  ) external view returns (PriceObservation[] memory prices);
-
-  function computeAverageEthForTokens(
-    address token,
-    uint256 tokenAmount,
-    uint256 minTimeElapsed,
-    uint256 maxTimeElapsed
-  ) external view returns (uint144);
-
-  function computeAverageTokensForEth(
-    address token,
-    uint256 wethAmount,
-    uint256 minTimeElapsed,
-    uint256 maxTimeElapsed
-  ) external view returns (uint144);
 }

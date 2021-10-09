4cee0ba9332871d2cc60359e4115432d94443fd6
d1ll0n <dillonkellar@protonmail.com>
2021-10-09 09:07:32 -0500

    Update solcover config for mainnet fork

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,15 @@
+require('dotenv').config()
+
 module.exports = {
-  norpc: true,
-  skipFiles: ["mocks/", "interfaces/", "test/"],
+  skipFiles: ["interfaces/", "test/", "libraries/"],
+  // Options for forking mainnet
+  providerOptions: {
+    allowUnlimitedContractSize: true,
+    host: "localhost",
+    port: 8545,
+    network_id: "13255220",
+    networkCheckTimeout: 60000,
+    fork: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+    fork_block_number: 13255220
+  }
 }

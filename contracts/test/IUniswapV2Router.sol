5460722a09c5cb22d53e6d5846d667e7f3e6d6fc
d1ll0n <dillonkellar@protonmail.com>
2021-10-09 09:05:49 -0500

    Add uniswap interfaces for tests

--- contracts/test/IUniswapV2Router.sol
+++ contracts/test/IUniswapV2Router.sol
@@ -8,4 +8,12 @@ interface IUniswapV2Router {
     address to,
     uint256 deadline
   ) external payable returns (uint256[] memory amounts);
+
+  function swapExactTokensForETH(
+    uint256 amountIn,
+    uint256 amountOutMin,
+    address[] calldata path,
+    address to,
+    uint256 deadline
+  ) external returns (uint256[] memory amounts);
 }
